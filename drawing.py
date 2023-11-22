df70ae01724cf4ce29bda4c3e686161062badaf9
Prajwal Deshmukh <prajwal.deshmukh@mitaoe.ac.in>
2023-11-22 14:17:02 +0530

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -1,28 +1,36 @@
-import streamlit as st
-import cv2
 import numpy as np
+import cv2
+import streamlit as st
 from collections import deque
 
-# Default called trackbar function
-def set_values(x):
-    print("")
+# Function to normalize and clip the image
+def normalize_and_clip(img):
+    return np.clip(img / 255.0, 0.0, 1.0)
 
-# Creating the trackbars needed for adjusting the marker colour
+# Streamlit setup
+st.title("Drawing App")
+
+# Create a Streamlit sidebar
 st.sidebar.title("Color Detectors")
-upper_hue = st.sidebar.slider("Upper Hue", 0, 180, 153, step=1, key="uh")
-upper_saturation = st.sidebar.slider("Upper Saturation", 0, 255, 255, step=1, key="us")
-upper_value = st.sidebar.slider("Upper Value", 0, 255, 255, step=1, key="uv")
-lower_hue = st.sidebar.slider("Lower Hue", 0, 180, 64, step=1, key="lh")
-lower_saturation = st.sidebar.slider("Lower Saturation", 0, 255, 72, step=1, key="ls")
-lower_value = st.sidebar.slider("Lower Value", 0, 255, 49, step=1, key="lv")
 
-# Giving different arrays to handle colour points of different colour
-bpoints = [deque(maxlen=1024)]
-gpoints = [deque(maxlen=1024)]
-rpoints = [deque(maxlen=1024)]
-ypoints = [deque(maxlen=1024)]
+# Trackbars for adjusting the marker color
+u_hue = st.sidebar.slider("Upper Hue", 0, 180, 153)
+u_saturation = st.sidebar.slider("Upper Saturation", 0, 255, 255)
+u_value = st.sidebar.slider("Upper Value", 0, 255, 255)
+l_hue = st.sidebar.slider("Lower Hue", 0, 180, 64)
+l_saturation = st.sidebar.slider("Lower Saturation", 0, 255, 72)
+l_value = st.sidebar.slider("Lower Value", 0, 255, 49)
 
-# These indexes will be used to mark the points in particular arrays of specific colour
+Upper_hsv = np.array([u_hue, u_saturation, u_value])
+Lower_hsv = np.array([l_hue, l_saturation, l_value])
+
+# Drawing setup
+bpoints = [deque(maxlen=512)]
+gpoints = [deque(maxlen=512)]
+rpoints = [deque(maxlen=512)]
+ypoints = [deque(maxlen=512)]
+
+# These indexes will be used to mark the points in particular arrays of specific color
 blue_index = 0
 green_index = 0
 red_index = 0
@@ -32,53 +40,110 @@ yellow_index = 0
 kernel = np.ones((5, 5), np.uint8)
 
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-color_index = 0
+colorIndex = 0
 
 # Here is code for Canvas setup
-paint_window = np.zeros((471, 636, 3)) + 255
-paint_window = cv2.rectangle(paint_window, (40, 1), (140, 65), (0, 0, 0), 2)
-paint_window = cv2.rectangle(paint_window, (160, 1), (255, 65), colors[0], -1)
-paint_window = cv2.rectangle(paint_window, (275, 1), (370, 65), colors[1], -1)
-paint_window = cv2.rectangle(paint_window, (390, 1), (485, 65), colors[2], -1)
-paint_window = cv2.rectangle(paint_window, (505, 1), (600, 65), colors[3], -1)
+paintWindow = np.zeros((471, 636, 3)) + 255
+paintWindow = cv2.rectangle(paintWindow, (40, 1), (140, 65), (0, 0, 0), 2)
+paintWindow = cv2.rectangle(paintWindow, (160, 1), (255, 65), colors[0], -1)
+paintWindow = cv2.rectangle(paintWindow, (275, 1), (370, 65), colors[1], -1)
+paintWindow = cv2.rectangle(paintWindow, (390, 1), (485, 65), colors[2], -1)
+paintWindow = cv2.rectangle(paintWindow, (505, 1), (600, 65), colors[3], -1)
 
-cv2.putText(paint_window, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paint_window, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(paint_window, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(paint_window, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-cv2.putText(paint_window, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150, 150, 150), 2, cv2.LINE_AA)
-st.sidebar.image(paint_window, channels="BGR", use_column_width=True)
+# Initialize Streamlit image placeholders
+frame_placeholder = st.empty()
+paint_window_placeholder = st.empty()
 
-# Creating the main Streamlit app layout
-st.title("Streamlit Drawing App")
-
-# Loading the default webcam of PC
+# Loading the default webcam of PC.
 cap = cv2.VideoCapture(0)
-
-# Function to get the current frame
-def get_frame():
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    return frame
-
-# Function to get the drawing window
-def get_drawing_window():
-    drawing_window = np.copy(paint_window)
-    return drawing_window
 
 # Keep looping
 while True:
-    frame = get_frame()
-    drawing_window = get_drawing_window()
+    # Reading the frame from the camera
+    ret, frame = cap.read()
+    # Flipping the frame to see the same side of yours
+    frame = cv2.flip(frame, 1)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Your existing color tracking logic...
+    # Identify the pointer by making its mask
+    Mask = cv2.inRange(hsv, Lower_hsv, Upper_hsv)
+    Mask = cv2.erode(Mask, kernel, iterations=1)
+    Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
+    Mask = cv2.dilate(Mask, kernel, iterations=1)
 
-    # Display the frame and drawing window
-    st.image(frame, channels="BGR", use_column_width=True, caption="Original Frame")
-    st.image(drawing_window, channels="BGR", use_column_width=True, caption="Drawing Window")
+    # Find contours for the pointer after identifying it
+    cnts, _ = cv2.findContours(Mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    center = None
 
-    # If the 'q' key is pressed, stop the application
+    # If the contours are formed
+    if len(cnts) > 0:
+        # Sorting the contours to find the biggest
+        cnt = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+        # Get the radius of the enclosing circle around the found contour
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        # Draw the circle around the contour
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        # Calculating the center of the detected contour
+        M = cv2.moments(cnt)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+
+        # Now checking if the user wants to click on any button above the screen
+        if center[1] <= 65:
+            if 40 <= center[0] <= 140:  # Clear Button
+                bpoints = [deque(maxlen=512)]
+                gpoints = [deque(maxlen=512)]
+                rpoints = [deque(maxlen=512)]
+                ypoints = [deque(maxlen=512)]
+
+                blue_index = 0
+                green_index = 0
+                red_index = 0
+                yellow_index = 0
+
+                paintWindow[67:, :, :] = 255
+            elif 160 <= center[0] <= 255:
+                colorIndex = 0  # Blue
+            elif 275 <= center[0] <= 370:
+                colorIndex = 1  # Green
+            elif 390 <= center[0] <= 485:
+                colorIndex = 2  # Red
+            elif 505 <= center[0] <= 600:
+                colorIndex = 3  # Yellow
+        else:
+            if colorIndex == 0:
+                bpoints[blue_index].appendleft(center)
+            elif colorIndex == 1:
+                gpoints[green_index].appendleft(center)
+            elif colorIndex == 2:
+                rpoints[red_index].appendleft(center)
+            elif colorIndex == 3:
+                ypoints[yellow_index].appendleft(center)
+    # Append the next deques when nothing is detected to avoid messing up
+    else:
+        bpoints.append(deque(maxlen=512))
+        blue_index += 1
+        gpoints.append(deque(maxlen=512))
+        green_index += 1
+        rpoints.append(deque(maxlen=512))
+        red_index += 1
+        ypoints.append(deque(maxlen=512))
+        yellow_index += 1
+
+    # Draw lines of all the colors on the canvas and frame
+    points = [bpoints, gpoints, rpoints, ypoints]
+    for i in range(len(points)):
+        for j in range(len(points[i])):
+            for k in range(1, len(points[i][j])):
+                if points[i][j][k - 1] is None or points[i][j][k] is None:
+                    continue
+                cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+
+    # Display the images in Streamlit
+    frame_placeholder.image(normalize_and_clip(frame), channels="BGR", use_column_width=True, clamp=True)
+    paint_window_placeholder.image(normalize_and_clip(paintWindow), channels="BGR", use_column_width=True, clamp=True)
+
+    # If the 'q' key is pressed then stop the application
     if st.button("Quit"):
         break
 
